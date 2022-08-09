b25d29bf69b09e0e77d4f7de6a55b7c64ccc5a7b
“lukusharma” <lukusharma@gmail.com>
2022-08-09 13:41:01 +0530

    changes

---
+++ Day 6/assignment-1.js
@@ -0,0 +1,5 @@
+var today = new Date();
+var date = today.getDate()+':'+(today.getMonth()+1)+':'+today.getFullYear();
+document.write("Today's Date: "+date+"<br>")
+var day = today.getDay();
+document.write("Today's Day: "+day)
---
+++ Day 6/assignment-2.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = "pink";
+var name = prompt("Enter Name");
+document.write(name);
+document.body.style.backgroundColor = "pink";
