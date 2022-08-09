a9af0eb21b6fe01a58224c50bee2c6fb335c789a
“lukusharma” <lukusharma@gmail.com>
2022-08-09 14:28:28 +0530

    changes

--- Day 6/assignment-1.js
+++ Day 6/assignment-1.js
@@ -1,5 +1,12 @@
 var today = new Date();
-var date = today.getDate()+':'+(today.getMonth()+1)+':'+today.getFullYear();
+const weekday = [   "Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"]
+var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
 document.write("Today's Date: "+date+"<br>")
-var day = today.getDay();
+var day = weekday[today.getDay()];
 document.write("Today's Day: "+day)
+document.body.style.backgroundColor="gray";
+document.body.style.fontSize="50px";
+document.body.style.textAlign="center";
+document.body.style.paddingTop="50px";
+document.body.style.color="cyan";
+document.body.style.fontStyle="cursive";
--- Day 6/assignment-3.js
+++ Day 6/assignment-3.js
@@ -8,3 +8,9 @@ var f = a - b;
  
 document.body.innerHTML="<h1 style = color:green>Addition is: "  + c +  "</h1>"  +  "<br>"  +  "<h1 style = color:green>Subtraction is: "  +  f  +  "</h1>"  +  "<br>"  +  "<h1 style = color:green>Multiplication is: "  + d +  "</h1>"  +  "<br>"  +  "<h1 style = color:green>Division is: "  + e +  "</h1>"  +  "<br>";
 document.body.style.backgroundColor = "pink";
+// document.body.style.backgroundColor="gray";
+// document.body.style.fontSize="50px";
+document.body.style.textAlign="center";
+// document.body.style.paddingTop="50px";
+document.body.style.color="cyan";
+document.body.style.fontStyle="cursive";
