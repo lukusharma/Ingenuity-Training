20ff274e25677447594570e27a660e4ecb0c5f5b
“lukusharma” <lukusharma@gmail.com>
2022-08-09 14:15:32 +0530

    day 6

---
+++ Day 6/assignment-3.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = "pink";
+var a = prompt("Enter First Number");
+var b = prompt("Enter Second Number");
+var c = a + b;
+var d = a * b;
+var e = a / b;
+var f = a - b;
+ 
+document.body.innerHTML="<h1 style = color:green>Addition is: "  + c +  "</h1>"  +  "<br>"  +  "<h1 style = color:green>Subtraction is: "  +  f  +  "</h1>"  +  "<br>"  +  "<h1 style = color:green>Multiplication is: "  + d +  "</h1>"  +  "<br>"  +  "<h1 style = color:green>Division is: "  + e +  "</h1>"  +  "<br>";
+document.body.style.backgroundColor = "pink";
